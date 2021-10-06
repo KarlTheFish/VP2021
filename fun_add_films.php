006066c75e1c1406d0b87845e7e527130ccb0472
KarlTheFish <karlvask@tlu.ee>
2021-10-06 11:44:50 +0300

    Varasemate kodutööde parandused

--- fun_add_films.php
+++ fun_add_films.php
@@ -7,47 +7,61 @@ require_once("fun_films.php");
 
 $film_store_notice = null;
 
-$user_title_input = null;
+$title_input_error = null;
+$year_input_error = null;
+$duration_input_error = null;
+$genre_input_error = null;
+$studio_input_error = null;
+$director_input_error = null;
 
 if(isset($_POST["film_submit"])){
 	if(!empty($_POST["title_input"])){
         $user_title_input = $_POST["title_input"];
-        if(!empty($_POST["year_input"])){
-            $user_year_input = $_POST["year_input"];
-            if(!empty($_POST["duration_input"])){
-                $user_duration_input = $_POST["duration_input"];
-                if(!empty($_POST["genre_input"])){
-                    $user_genre_input = $_POST["genre_input"];
-                    if(!empty($_POST["studio_input"])){
-                        $user_studio_input = $_POST["studio_input"];
-                        if(!empty($_POST["director_input"])){
-                            $user_director_input = $_POST["director_input"];
-                            $film_store_notice = store_film($_POST["title_input"],$_POST["year_input"],$_POST["duration_input"],$_POST["genre_input"],$_POST["studio_input"],$_POST["director_input"]);
-                        }
-                        else{
-                        $film_store_notice = "Sisestage filmi tootja!";
-                        }
-                    }
-                    else{
-                        $film_store_notice = "Sisestage filmistuudio nimi!";
-                    }
-                }
-                else{
-                    $film_store_notice = "Sisestage filmi zanr!";
-                }
-            }
-            else{
-                $film_store_notice = "Sisestage filmi kestus!";
-            }
-        }    
-        else {
-            $film_store_notice = "Sisestage valmimisaasta!";
-        }
     }
-	else {
-		$film_store_notice = "Sisestage pealkiri!";
-	}
-}
+    else{
+        $film_store_notice = "Andmeid on puudu!";
+        $title_input_error = "Sisestage filmi pealkiri!";
+    }
+    if(!empty($_POST["year_input"])){
+        $user_year_input = $_POST["year_input"];
+    }
+    else {
+    $film_store_notice = "Andmeid on puudu!";
+    $year_input_error = "Sisestage väljastamisaasta!";
+    }
+    if(!empty($_POST["duration_input"])){
+        $user_duration_input = $_POST["duration_input"];
+    }
+    else{
+        $film_store_notice = "Andmeid on puudu!";
+        $duration_input_error = "Sisestage filmi kestus minutites!";
+    }
+    if(!empty($_POST["genre_input"])){
+        $user_genre_input = $_POST["genre_input"];
+    }
+    else{
+        $film_store_notice = "Andmeid on puudu!";
+        $genre_input_error = "Sisestage filmizanr!";
+    }
+    if(!empty($_POST["studio_input"])){
+        $user_studio_input = $_POST["studio_input"];
+    }
+    else{
+        $film_store_notice = "Andmeid on puudu!";
+        $studio_input_error = "Sisestage filmistuudio!";
+    }
+    if(!empty($_POST["director_input"])){
+        $user_director_input = $_POST["director_input"];
+    }
+    else{
+    $film_store_notice = "Andmeid on puudu!";
+    $director_input_error = "Sisestage filmi lavastaja!";
+    }
+    if(empty($title_input_error) and empty($year_input_error) and empty($duration_input_error) and empty($genre_input_error) and empty($studio_input_error) and empty($director_input_error)){
+        $film_store_notice = store_film($_POST["title_input"],$_POST["year_input"],$_POST["duration_input"],$_POST["genre_input"],$_POST["studio_input"],$_POST["director_input"]);
+    }
+}    
+
 
 
 ?>
@@ -90,25 +104,30 @@ if(isset($_POST["film_submit"])){
 
 	<h1><center><img src="banana.gif" alt="tantsiv banaan" width=100></img> <?php echo $author_name;?>i veebileht <img src="banana.gif" alt="tantsiv banaan" width=100></img></center></h1>
 	<h2><center>Eesti filmid</center></h2>
-	
    <form method="POST">
         <label for="title_input">Filmi pealkiri</label>
         <input type="text" name="title_input" id="title_input" placeholder="filmi pealkiri">
+        <?php echo $title_input_error ?>
         <br>
         <label for="year_input">Valmimisaasta</label>
         <input type="number" name="year_input" id="year_input" min="1912" value="">
+        <?php echo $year_input_error ?>
         <br>
         <label for="duration_input">Kestus</label>
         <input type="number" name="duration_input" id="duration_input" min="1" value="60" max="600">
+        <?php echo $duration_input_error ?>
         <br>
         <label for="genre_input">Filmi žanr</label>
         <input type="text" name="genre_input" id="genre_input" placeholder="žanr">
+        <?php echo $genre_input_error ?>
         <br>
         <label for="studio_input">Filmi tootja</label>
         <input type="text" name="studio_input" id="studio_input" placeholder="filmi tootja">
+        <?php echo $studio_input_error ?>
         <br>
         <label for="director_input">Filmi režissöör</label>
         <input type="text" name="director_input" id="director_input" placeholder="filmi režissöör">
+        <?php echo $director_input_error ?>
         <br>
         <input type="submit" name="film_submit" value="Salvesta">
     </form>
--- page2.php
+++ page2.php
@@ -3,53 +3,53 @@
 $author_name = "Karl"; #PHP lause peab löppema semikooloniga
 //juhusliku foto lisamine
 $photo_dir = "pildid/";
-#$photo_files = scandir($photo_dir); #failikataloogi skännimiseks scandir
-$real_files = array_slice(scandir($photo_dir), 2);
+$all_files = scandir($photo_dir); #failikataloogi skännimiseks scandir
+$real_files = array_slice($all_files, 2);
 
 //sõelume välja päris pildid
-/* $photos = [];
-$allowed_files = ["image/png","image/jpeg"];
-foreach($real_files as $file_name) {
-	$file_meta = getimagesize($photo_dir .$photo_files);
-	if(isset($file_meta["mime"])) {
-		if(in_array($file_info["mime"], $allowed_files)) {
-			array_push($allowed_files, $file_name);
-		}
-	}
-} */
+$photo_files = [];
+	$allowed_photo_types = ["image/jpeg","image/png"];
+	foreach($real_files as $file_name){
+		$file_info = getimagesize($photo_dir .$file_name);
+		if(isset($file_info["mime"])){
+			if(in_array($file_info["mime"], $allowed_photo_types)){
+				array_push($photo_files, $file_name);
+			} //if in_array
+		} //if isset lõppeb
+	} //foreach lõppes made by Rinde aitäh Rinde
 
 //suvalise foto valimine
 
-$limit = count($real_files);
+$limit = count($photo_files);
 $pic_nr = mt_rand(0, $limit - 1);
-$ran_picture = $real_files[$pic_nr];
+$ran_picture = $photo_files[$pic_nr];
 $photo_html = '<img src="' .$photo_dir .$ran_picture . '" alt="Pilt" width="500">';
-
-//fotode valikumenüü
-$photo_select_html = "\n".'<select name="photo_select">'."\n";
-	for($i = 0; $i < $limit; $i++ ){
-	$photo_select_html .= '<option value="' .$i .'">' .$real_files[$i]. "</option> \n";
-	if($i == $pic_nr){
-		$photo_select_html .= '<option value="' .$i .'" selected>' .$real_files[$i]. "</option> \n";
-		}
-	}
-	$photo_select_html .= "</select>";
 
 //fotode valiku nupu vajutamise kontroll
 
 $photo_select_test = null;
 $user_photo_select = null;
 if(isset($_POST["submit_picture"])){
-	$photo_select_test = "Nupp vajutatud";
 	$user_photo_select = $_POST["photo_select"];
-	$ran_picture = $real_files[$user_photo_select];
+	$ran_picture = $photo_files[$user_photo_select];
 	$pic_nr = $user_photo_select;
 	$photo_html = '<img src="' .$photo_dir .$ran_picture . '" alt="Valitud pilt" width="500">';
 }
+//fotode valikumenüü
+$photo_select_html = "\n".'<select name="photo_select">'."\n";
+	for($i = 0; $i < $limit; $i++ ){
+	if($i == $pic_nr){
+		$photo_select_html .= '<option value="' .$i .'" selected>' .$photo_files[$i]. "</option> \n";
+		}
+    else{
+    $photo_select_html .= '<option value="' .$i .'">' .$photo_files[$i]. "</option> \n";
+    }
+	}
+	$photo_select_html .= "</select>";
 
 //nimi fotode all
 
-//$photo_list_html = "\n".$ran_picture."\n";
+$photo_list_html = $ran_picture."<br>";
 
 ?>
 <!DOCTYPE html> <!-- Vajalik HTML osa alguses -->
@@ -108,17 +108,11 @@ if(isset($_POST["submit_opinion"]))
 	<h1><center><img src="banana.gif" alt="tantsiv banaan" width=100></img> <?php echo $author_name;?>i veebileht <img src="banana.gif" alt="tantsiv banaan" width=100></img></center></h1>
 	<h2><center>Graphic design is my passion</center></h2>
 	<form method="post">
-	<?php echo $photo_html; 
+	<?php echo $photo_html."<br>";
 	echo $photo_list_html;
 	echo $photo_select_html; 
 	echo $user_photo_select; ?>
 	<input type="submit" name="submit_picture" value="Vali pilt">
-		<span><?php echo $photo_select_test ?></span>
-        
-        <select name="test">
-        <option value="0">Test1</option>
-        <option value="1" selected>Test2</option>
-        </select>
 	</form>
 	<form method="post"> <!-- vormielement -->
 		<hr>
